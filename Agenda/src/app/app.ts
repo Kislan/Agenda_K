18755fb181f89e0b06fc59ee1d3165f192080dd7
Kislan <Kislan.lima@escolar.ifrn.edu.br>
2026-07-06 11:35:41 -0300

    cabecalho

--- Agenda/src/app/app.ts
+++ Agenda/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { AdicionaContato } from './adiciona-contato/adiciona-contato';
 import { ExibeContatos } from './exibe-contatos/exibe-contatos';
 import { FiltroContato } from './filtro-contato/filtro-contato';
+import { Cabecalho } from './cabecalho/cabecalho';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AdicionaContato, ExibeContatos, FiltroContato],
+  imports: [RouterOutlet, AdicionaContato, ExibeContatos, FiltroContato, Cabecalho],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
